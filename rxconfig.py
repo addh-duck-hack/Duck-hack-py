d5af2d6f2a9473521df47d481481178b9968566b
Adrian Jacobo <adrian.jacobo@duck-hack.com>
2024-04-05 20:47:01 +0000

    Se modifica la configuracion para poder ejecutarlo en produccion, tambien se agregan los archivos de docker para crear y publicar las imagenes

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="duck_hack",
-    api_url="https://duck-hack.cloud"
+    api_url="https://api.duck-hack.cloud",
+    frontend_port="82",
+    backend_port="83",
+    deploy_url="https://duck-hack.cloud"
 )
