926cd33904291fc5be6f5577feef828a220bddd6
root <root@duck-hack.com>
2024-09-19 02:11:14 +0000

    Se cambian puertos y urls finales

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="duck_hack",
     api_url="https://api.duck-hack.cloud",
-    frontend_port="82",
-    backend_port="83",
-    deploy_url="https://prod.duck-hack.cloud"
+    frontend_port="3000",
+    backend_port="8000",
+    deploy_url="https://mx.duck-hack.cloud"
 )
